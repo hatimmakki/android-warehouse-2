f1c61ce68ec7794f6e588bd5fd77119727d9301e
Maxwell <maxib2007@gmail.com>
2016-08-26 17:53:03 -0300

    Parallax Demo

---
+++ app/src/main/java/com/maxwell/androidwarehouse2/activities/ParallaxDemo.java
@@ -0,0 +1,18 @@
+package com.maxwell.androidwarehouse2.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.maxwell.androidwarehouse2.R;
+
+/**
+ * Created by MauroLombardi on 9/16/15.
+ */
+public class ParallaxDemo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.parallax_layout);
+    }
+}
