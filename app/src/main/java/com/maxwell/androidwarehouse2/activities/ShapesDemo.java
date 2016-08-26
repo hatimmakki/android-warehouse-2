eab18fa146793169829dd0459a45ff00578ca360
Maxwell <maxib2007@gmail.com>
2016-08-26 16:34:46 -0300

    Shapes Demo

---
+++ app/src/main/java/com/maxwell/androidwarehouse2/activities/ShapesDemo.java
@@ -0,0 +1,18 @@
+package com.maxwell.androidwarehouse2.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.maxwell.androidwarehouse2.R;
+
+/**
+ * Created by Maxwell on 11/12/2015.
+ */
+public class ShapesDemo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.shapes_layout);
+    }
+}
